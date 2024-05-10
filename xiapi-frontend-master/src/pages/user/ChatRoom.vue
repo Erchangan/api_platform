fe8ca2359826e1fed9da46cdd8563c974881575c
k2 <497452617@qq.com>
2024-05-10 21:37:13 +0800

    管理员不显示自己

--- xiapi-frontend-master/src/pages/user/ChatRoom.vue
+++ xiapi-frontend-master/src/pages/user/ChatRoom.vue
@@ -71,7 +71,6 @@ const sendMessage = () => {
 const getAllLoginUser = async () => {
   const res = await request.get('user/getAllLoginUser')
   if (res.code == 0) {
-    debugger
     let filterList = res.data.filter(item=>item.userRole != 'admin');
     let resWithSelected = filterList.map(item => ({...item, selected: false}));
     allLoginUser.value = resWithSelected
